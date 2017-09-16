eaa04c8c7389eaf05a7f9ac58f9584f7fda36807
Cindy Zhou <loiralae@users.noreply.github.com>
2017-09-16 10:10:15 -0400

    Add firebase (#13)

--- groupr-web/src/App.js
+++ groupr-web/src/App.js
@@ -3,8 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 import Swiper from './Swiper';
 import HackerProfile from './HackerProfile'
+var firebase = require("firebase");
 
 class App extends Component {
+  constructor() {
+    super();
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyB0g6P9u0M3k-gO5E3oxjjHlnqRIwK85Nw",
+      authDomain: "groupr-9399b.firebaseapp.com",
+      databaseURL: "https://groupr-9399b.firebaseio.com",
+      projectId: "groupr-9399b",
+      storageBucket: "groupr-9399b.appspot.com",
+      messagingSenderId: "580456477433"
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     let sampleData = [(
       <HackerProfile
