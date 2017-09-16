db1d118bb5b472d9a35cc9abc2bc5412cc58ac49
eddyionescu <eddyionescu@gmail.com>
2017-09-16 14:40:23 -0400

    firebase

--- groupr-web/src/App.js
+++ groupr-web/src/App.js
@@ -5,20 +5,11 @@ import Swiper from './Swiper';
 import HackerProfile from './HackerProfile';
 import firebase from 'firebase';
 import TeamCreate from './TeamCreate';
+import ReactFireMixin from 'reactfire';
 
-class App extends Component {
+class App extends ReactFireMixin(Component) {
   constructor() {
     super();
-    // Initialize Firebase
-    const config = {
-      apiKey: "AIzaSyB0g6P9u0M3k-gO5E3oxjjHlnqRIwK85Nw",
-      authDomain: "groupr-9399b.firebaseapp.com",
-      databaseURL: "https://groupr-9399b.firebaseio.com",
-      projectId: "groupr-9399b",
-      storageBucket: "groupr-9399b.appspot.com",
-      messagingSenderId: "580456477433"
-    };
-    firebase.initializeApp(config);
   }
 
   render() {
