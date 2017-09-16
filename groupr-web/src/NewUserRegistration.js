70e46f9ebabbb51c675f29a6b301386e138296e6
forbesb <magnos.cartus@gmail.com>
2017-09-16 13:51:44 -0400

    New User Reg

--- groupr-web/src/NewUserRegistration.js
+++ groupr-web/src/NewUserRegistration.js
@@ -3,17 +3,28 @@ import React, { Component } from 'react';
 import firebase from 'firebase';
 
 type Props = {
+<<<<<<< HEAD
     callback: ()=>void
 };
 
 export default class NewUserRegistration extends Component {
+=======
+    
+};
+
+export default class TeamCreate extends Component {
+>>>>>>> New User Reg
 
     props: Props;
 
     constructor(props) {
         super(props);
         this.state = {
+<<<<<<< HEAD
             bio: ''
+=======
+            bio: '',
+>>>>>>> New User Reg
         };
 
         this.handlechange=this.handlechange.bind(this);
@@ -43,6 +54,7 @@ export default class NewUserRegistration extends Component {
                 uid: providerData.uid,
                 name: providerData.displayName,
                 description: this.state.bio,
+<<<<<<< HEAD
                 pic: providerData.photoUrl == null ? "" : providerData.photoUrl
              }
             var newUserId = firebase.database().ref().child('users').push().key;
@@ -51,12 +63,24 @@ export default class NewUserRegistration extends Component {
             firebase.database().ref('users/'+newUserId).set(newUserData);
 
             this.props.callback();
+=======
+                pic: providerData.photoUrl
+             }
+            var newGroupId = firebase.database.ref().child('groups').push().key;
+               
+
+            firebase.database.ref('users/'+newGroupId).set(newTeamData);
+>>>>>>> New User Reg
         } else {
             alert("Invalid user");
         }
     }
 
     render() {
+<<<<<<< HEAD
+=======
+        const {name, description} = this.props;
+>>>>>>> New User Reg
         return(
             <div>
                 <form name="newUserRegForm" onSubmit={this.handleSubmit}>
