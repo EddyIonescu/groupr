482ce2bf49285c29c6d07a691f5b869be77132dd
Eddy Ionescu <eddyionescu@gmail.com>
2017-09-16 20:37:47 -0700

    website design improvements (#24)
    what the heck's website ui changes

--- groupr-web/src/App.js
+++ groupr-web/src/App.js
@@ -111,8 +111,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-header jumbotron my-blue-class">
           <h2>Welcome to Groupr</h2>
         </div>
         <p className="App-intro">
